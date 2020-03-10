45a18f1176df88d1f592ce0a7359448f8ecc58ae
Pepe García <pepe@pepegar.com>
2020-03-10 01:04:50 +0100

    solve exercise

--- exercise-2-form-validation/js/main.js
+++ exercise-2-form-validation/js/main.js
@@ -5,9 +5,33 @@
 let email = document.querySelector("#email");
 
 let validateEmail = (e) => {
-    console.log(e.target.value);
+    let currentEmail = e.target.value;
+
+    if (!currentEmail.includes("@")) {
+        email.classList.add("wrong");
+    } else {
+        email.classList.remove("wrong");
+    }
 };
 
 email.addEventListener("keyup", validateEmail);
+
 // validate telephone only contains numbers.  If it doesn't, make the border red
+let telephone = document.querySelector("#telephone");
+
+let validateTelephone = (e) => {
+    let telephoneRegex = /^[0-9]$/;
+    let currentTelephone = e.target.value;
+
+    if (!currentTelephone.match(telephoneRegex)) {
+        telephone.classList.add("wrong");
+    } else {
+        telephone.classList.remove("wrong");
+    }
+};
+
+telephone.addEventListener("keyup", validateTelephone);
+
+
+
 // validate city == Madrid.  If it doesn't, make the border red
