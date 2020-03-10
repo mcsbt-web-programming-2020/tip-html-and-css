de57c282093431d389ff46a433c0578554a8995e
Pepe García <pepe@pepegar.com>
2020-03-10 01:06:16 +0100

    solve exercise

--- exercise-2-form-validation/js/main.js
+++ exercise-2-form-validation/js/main.js
@@ -4,34 +4,7 @@
 // validate email contains an @ sign.  If it doesn't, make the border red
 let email = document.querySelector("#email");
 
-let validateEmail = (e) => {
-    let currentEmail = e.target.value;
-
-    if (!currentEmail.includes("@")) {
-        email.classList.add("wrong");
-    } else {
-        email.classList.remove("wrong");
-    }
-};
-
-email.addEventListener("keyup", validateEmail);
+email.addEventListener("keyup", () => console.log("heyya!"));
 
 // validate telephone only contains numbers.  If it doesn't, make the border red
-let telephone = document.querySelector("#telephone");
-
-let validateTelephone = (e) => {
-    let telephoneRegex = /^[0-9]$/;
-    let currentTelephone = e.target.value;
-
-    if (!currentTelephone.match(telephoneRegex)) {
-        telephone.classList.add("wrong");
-    } else {
-        telephone.classList.remove("wrong");
-    }
-};
-
-telephone.addEventListener("keyup", validateTelephone);
-
-
-
 // validate city == Madrid.  If it doesn't, make the border red
